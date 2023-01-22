80670e70a8b8b9e9789e0d18bc09dab6b3d81135
tiagoalex2002 <tiagoalex0202@gmail.com>
2023-01-22 15:02:03 -0300

    ajustes

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function login() {
     };
 
     const promiseNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', obj);
-    promiseNome.then(suc);
-    promiseNome.catch(error);
+    promiseNome.then(deuCerto);
+    promiseNome.catch(deuErrado);
 
 }
 
@@ -37,32 +37,29 @@ function instavel(resposta) {
 }
 
 
-function error(resposta) {
+function deuErrado(resposta) {
     const statusError = resposta.response.status;
-    if (statusError == 400) {
+    if (statusError === 400) {
         alert("Nome de usuário em uso. Por favor, escolher outro");
-        login();
-    }
-    else {
-        alert("Erro de Conexão");
         login();
     }
     window.location.reload();
 }
 
-function suc(resposta) {
+function deuCerto(resposta) {
     const status = resposta.status;
-    if (status == 200) {
-        conexao(obj);
-        obtermensagens();
+    const usuario = { name: nome };
+    if (status === 200) {
+        conexao(usuario);
+        obterMensagens();
     }
 }
 
-function obtermensagens() {
+function obterMensagens() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promise.then(chegada);
     promise.catch(falha);
-    setTimeout((obtermensagens), 3000);
+    setTimeout((obterMensagens), 3000);
 
 }
 
@@ -83,27 +80,27 @@ function exibirMensagens() {
     for (let i = 0; i < mensagens.length; i++) {
         if (mensagens[i].type === "message") {
             let template = `<li class="mensagemNormal" data-test="message">
-            <span style="color:#AAAAAA">(${mensagens[cont].time})</span>&nbsp<span style="font-weight:700">${mensagens[cont].from}</span>&nbsppara&nbsp<span style="font-weight:700">${mensagens[cont].to}</span>: ${mensagens[cont].text} </li>`;
+            <span style="color:#AAAAAA">(${mensagens[i].time})</span>&nbsp<span style="font-weight:700">${mensagens[i].from}</span>&nbsppara&nbsp<span style="font-weight:700">${mensagens[i].to}</span>: ${mensagens[i].text} </li>`;
             listaMensagens.innerHTML = listaMensagens.innerHTML + template;
             listaMensagens.lastChild.scrollIntoView();
         }
         else if (mensagens[i].type === "status") {
-            let template = `<li class="tipoStatus" data-test="message">
-            <span style="color:#AAAAAA">(${mensagens[cont].time})</span>&nbsp<span style="font-weight:700">${mensagens[cont].from}</span>&nbsp${mensagens[cont].text} </li>`;
+            let template = `<li class="mensagemStatus" data-test="message">
+            <span style="color:#AAAAAA">(${mensagens[i].time})</span>&nbsp<span style="font-weight:700">${mensagens[i].from}</span>&nbsp${mensagens[i].text} </li>`;
             listaMensagens.innerHTML = listaMensagens.innerHTML + template;
             listaMensagens.lastChild.scrollIntoView();
         }
         else if (mensagens[i].type === "private_message") {
-            let template = `<li class="tipoMensagemPrivada" data-test="message">
-            <span style="color:#AAAAAA">(${mensagens[cont].time})</span>&nbsp<span style="font-weight:700">${mensagens[cont].from}</span>&nbspreservadamente&nbsppara&nbsp<span style="font-weight:700">${mensagens[cont].to}</span>: ${mensagens[cont].text}</li>`;
+            let template = `<li class="mensagemPrivada" data-test="message">
+            <span style="color:#AAAAAA">(${mensagens[i].time})</span>&nbsp<span style="font-weight:700">${mensagens[i].from}</span>&nbspreservadamente&nbsppara&nbsp<span style="font-weight:700">${mensagens[i].to}</span>: ${mensagens[i].text}</li>`;
             listaMensagens.innerHTML = listaMensagens.innerHTML + template;
             listaMensagens.lastChild.scrollIntoView();
         }
     }
 }
 
-function enviarMensagens() {
-    let mensagem = document.querySelector("input");
+function enviarMensagem() {
+    let mensagem = document.querySelector('input');
     let obj2 = {
         from: nome,
         to: "Todos",
@@ -113,17 +110,15 @@ function enviarMensagens() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", obj2);
     promise.then(mensagemEnviada);
     promise.catch(mensagemNaoEnviada);
-    mensagem.value = "";
+    mensagem.value = '';
 }
 
 function mensagemEnviada(resposta) {
     console.log("Mensagem Enviada com sucesso")
-    obtermensagens();
+    obterMensagens();
 }
 
 function mensagemNaoEnviada(resposta) {
     console.log("Houve falha no envio")
     window.location.reload();
 }
-
-
